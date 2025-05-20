2fa36590547fca1fd0b62a20de1271884db7253d
rvsmooth <riveeks.smooth@gmail.com>
2025-05-20 06:15:07 +0530

    Add README and user.js

---
+++ README.md
@@ -0,0 +1,6 @@
+Firefox user.js 
+
+A repo just to store my customized version of user.js for firefox 
+
+### Credits
+- Betterfox (for some configs)
---
+++ user.js
@@ -0,0 +1,36 @@
+/** TRACKING PROTECTION ***/
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.download.start_downloads_in_tmp_dir", true);
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+user_pref("browser.uitour.enabled", false);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.profiles.enabled", true);
+user_pref("browser.aboutConfig.showWarning", false);
+
+/** MOZILLA ***/
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("geo.provider.network.url", "https://beacondb.net/v1/geolocate");
+user_pref("browser.search.update", false);
+user_pref("permissions.manager.defaultsUrl", "");
+
+/** THEME ADJUSTMENTS ***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.privateWindowSeparation.enabled", false); // WINDOWS
+
+/** NEW TAB PAGE ***/
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+
+/** POCKET ***/
+user_pref("extensions.pocket.enabled", false);
+
+/** HARDWARE ACCELERATION **/
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.av1.enabled", false);
+user_pref("media.mediasource.vp9.enabled", false);
+user_pref("media.webm.enabled", false);
